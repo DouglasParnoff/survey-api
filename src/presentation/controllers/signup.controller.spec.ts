a7d81fde8f278fe48a0d0bf4e748ee42195fc8b7
Douglas Parnoff <dougparnoff@gmail.com>
2023-01-27 14:12:41 +0000

    test: add signup controller it returns just 400 for now

--- src/presentation/controllers/signup.controller.spec.ts
+++ src/presentation/controllers/signup.controller.spec.ts
@@ -1,3 +1,17 @@
+import { SignUpController } from './signup'
+
 describe("SignUp Controller", () => {
-    
+    test("Should return 400 if no name is provided", () =>{
+        const sut = new SignUpController()
+        const httpRequest = {
+            body: {
+                //name: 'the test'
+                email: 'e@mail.com',
+                password: 'anypwd',
+                passwordConfirmation: 'anypwd'
+            }
+        }
+        const httpResponse = sut.handle(httpRequest)
+        expect(httpResponse.statusCode).toBe(400)
+    })
 })
