bf3e6010d571eea759cf4089b9f98154b4363f3e
Douglas Parnoff <dougparnoff@gmail.com>
2023-01-27 15:16:39 +0000

    refactor: add custom errors

--- src/presentation/controllers/signup.controller.spec.ts
+++ src/presentation/controllers/signup.controller.spec.ts
@@ -1,9 +1,12 @@
 import { SignUpController } from './signup'
+import { HttpRequest } from '../protocols/http'
+import { MissingParamError } from '../errors/missing-param-error' 
 
 describe("SignUp Controller", () => {
     test("Should return 400 if no name is provided", () =>{
         const sut = new SignUpController()
-        const httpRequest = {
+        const httpRequest: HttpRequest = {
+            statusCode: 400,
             body: {
                 //name: 'the test'
                 email: 'e@mail.com',
@@ -13,6 +16,23 @@ describe("SignUp Controller", () => {
         }
         const httpResponse = sut.handle(httpRequest)
         expect(httpResponse.statusCode).toBe(400)
-        expect(httpResponse.body).toEqual(new Error('Missing param: name'))
+        expect(httpResponse.body).toEqual(new MissingParamError('name'))
+        expect(httpResponse.body instanceof MissingParamError).toBe(true)
+    })
+    
+    test("Should return 400 if no email is provided", () =>{
+        const sut = new SignUpController()
+        const httpRequest: HttpRequest = {
+            statusCode: 400,
+            body: {
+                name: 'the name',
+                //email: 'the test',
+                password: 'anypwd',
+                passwordConfirmation: 'anypwd'
+            }
+        }
+        const httpResponse = sut.handle(httpRequest)
+        expect(httpResponse.statusCode).toBe(400)
+        expect(httpResponse.body).toEqual(new MissingParamError('email'))
     })
 })
--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,8 +1,17 @@
+import { HttpRequest, HttpResponse } from '../protocols/http'
+import { MissingParamError } from '../errors/missing-param-error'
+
 export class SignUpController {
-    handle (httpRequest: any): any {
+    handle (httpRequest: HttpRequest): HttpResponse {
+        if(!httpRequest.body.name){
+            return { 
+                statusCode: 400,
+                body: new MissingParamError('name')
+            }            
+        }
         return { 
             statusCode: 400,
-            body: new Error('Missing param: name')
+            body: new MissingParamError('email')
         }
     }
 }
