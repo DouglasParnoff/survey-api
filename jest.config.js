6517c6b5f3f73ca5884526c35da7612228502640
EC2 Default User <ec2-user@ip-172-31-2-115.ec2.internal>
2023-01-27 12:21:08 +0000

    chore: add jest for unit tests

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+/*
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+module.exports = {
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  // Indicates whether the coverage information should be collected while executing the test
+  collectCoverage: true,
+  // The directory where Jest should output its coverage files
+  coverageDirectory: "coverage",
+  // Indicates which provider should be used to instrument code for coverage
+  coverageProvider: "v8",
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+};
